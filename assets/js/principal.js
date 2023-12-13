e84ca1fb27f78dc34279ad6665635a6ee4a20b49
Romaa83 <agustin_roma@outlook.com>
2023-12-13 14:26:08 -0300

    filtrado

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -21,8 +21,43 @@ tienda.articulos.forEach(elemento => {
 </div>`
 });
 
+contenedor.innerHTML = ContenidoHTML
 
- contenedor.innerHTML = ContenidoHTML
+function filtrarPorCategoria (categoria) {
+    let ContenidoHTML = ``;
+    tienda.articulos.forEach(elemento => {
+        if (elemento.categoria === categoria)
+        ContenidoHTML += `<div class="tarjeta_producto" id="tarjeta_producto" data-categoria="periferico">
+        <div class="producto" id="producto">
+            <img src="${elemento.imagen}" alt="Teclado Redragon">
+            <div class="body_producto">
+                <h3>${elemento.nombre}</h3>
+                <p>${elemento.precio}</p>
+            </div>
+            <div class="carro">
+                <button class="agregar-carrito">Agregar</button>
+            </div>
+        </div>
+    </div>`
+
+    contenedor.innerHTML = ContenidoHTML
+    })
+}
+
+document.getElementById("btnPerifericos").addEventListener("click", function() {
+    filtrarPorCategoria('periferico');
+});
+
+document.getElementById("btnHardware").addEventListener("click", function() {
+    filtrarPorCategoria('hardware');
+});
+
+document.getElementById("btnMonitores").addEventListener("click", function() {
+    filtrarPorCategoria('monitores');
+});
+
+
+
 
 
 
